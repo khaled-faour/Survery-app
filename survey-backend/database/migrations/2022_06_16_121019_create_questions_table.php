ac3aaf8fe9c5f3595f4739b8258bb44064bfc3fe
Khaled Faour <khaledfaour2@gmail.com>
2022-06-16 15:16:37 +0300

    Added Question Model and Table

---
+++ survey-backend/database/migrations/2022_06_16_121019_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->foreignID('survey_id')->constrained('surveys');
+            $table->string('type');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
